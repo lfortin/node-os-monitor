e809ff5d60caeeb8039c21a6f34de15cda579a3a
Laurent Fortin <laurent.fortin@gmail.com>
2015-08-16 00:06:58 -0400

    new version 1.0.4

--- os-monitor.js
+++ os-monitor.js
@@ -56,7 +56,7 @@ var Monitor = function() {
 
 util.inherits(Monitor, stream.Readable);
 
-Monitor.prototype.version = '1.0.3';
+Monitor.prototype.version = '1.0.4';
 
 
 // readable stream implementation requirement
