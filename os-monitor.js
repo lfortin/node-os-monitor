014ac3eb8520184587de81d77635aea1eee5c480
Laurent Fortin <laurent.fortin@gmail.com>
2019-06-06 00:17:39 -0400

    added TypeScript code

--- os-monitor.js
+++ os-monitor.js
@@ -26,7 +26,7 @@ var util = require('util'), os = require('os'), stream = require('readable-strea
 // constructor
 var Monitor = /** @class */ (function (_super) {
     __extends(Monitor, _super);
-    function Monitor(options) {
+    function Monitor() {
         var _this = _super.call(this, { highWaterMark: 102400 }) || this;
         _this._monitorState = {
             running: false,
@@ -37,145 +37,144 @@ var Monitor = /** @class */ (function (_super) {
         };
         return _this;
     }
+    Monitor.prototype.sendEvent = function (event, obj) {
+        var eventObject = _.extend({ type: event, timestamp: Math.floor(_.now() / 1000) }, obj);
+        // for EventEmitter
+        this.emit(event, eventObject);
+        // for readable Stream
+        if (this.config().stream && this._monitorState.streamBuffering) {
+            var prettyJSON = os.EOL + JSON.stringify(eventObject, null, 2);
+            if (!this.push(prettyJSON)) {
+                this._monitorState.streamBuffering = false;
+            }
+        }
+    };
+    Monitor.prototype._isEnded = function () {
+        return !!this._monitorState.ended;
+    };
+    Monitor.prototype.start = function (options) {
+        var _this = this;
+        if (this._isEnded()) {
+            throw new Error("monitor has been ended by .destroy() method");
+        }
+        this.stop()
+            .config(options);
+        var cycle = function () {
+            var info = {
+                loadavg: os.loadavg(),
+                uptime: os.uptime(),
+                freemem: os.freemem(),
+                totalmem: os.totalmem()
+            }, config = _this.config(), freemem = (config.freemem < 1) ? config.freemem * info.totalmem : config.freemem;
+            if (!config.silent) {
+                _this.sendEvent('monitor', info);
+            }
+            if (info.loadavg[0] > config.critical1) {
+                _this.sendEvent('loadavg1', info);
+            }
+            if (info.loadavg[1] > config.critical5) {
+                _this.sendEvent('loadavg5', info);
+            }
+            if (info.loadavg[2] > config.critical15) {
+                _this.sendEvent('loadavg15', info);
+            }
+            if (info.freemem < freemem) {
+                _this.sendEvent('freemem', info);
+            }
+            if (Number(config.uptime) && info.uptime > Number(config.uptime)) {
+                _this.sendEvent('uptime', info);
+            }
+        };
+        if (this.config().immediate) {
+            process.nextTick(cycle);
+        }
+        this._monitorState.interval = setInterval(cycle, this.config().delay);
+        if (!this.isRunning()) {
+            this._monitorState.running = true;
+            this.sendEvent('start');
+        }
+        return this;
+    };
+    Monitor.prototype.stop = function () {
+        clearInterval(this._monitorState.interval);
+        if (this.isRunning()) {
+            this._monitorState.running = false;
+            this.sendEvent('stop');
+        }
+        return this;
+    };
+    Monitor.prototype.reset = function () {
+        this.sendEvent('reset');
+        this[this.isRunning() ? 'start' : 'config'](_.clone(defaults));
+        return this;
+    };
+    Monitor.prototype.destroy = function (err) {
+        if (!this._isEnded()) {
+            this.sendEvent('destroy');
+            this.stop();
+            if (this instanceof stream.Readable) {
+                this.emit('close');
+                if (_.isFunction(stream.Readable.prototype.destroy)) {
+                    stream.Readable.prototype.destroy.apply(this, [err]);
+                }
+                this.push(null);
+            }
+            this._monitorState.ended = true;
+        }
+        return this;
+    };
+    Monitor.prototype.config = function (options) {
+        if (_.isObject(options)) {
+            _.extend(this._monitorState.config, options);
+            this.sendEvent('config', { options: _.clone(options) });
+        }
+        return this._monitorState.config;
+    };
+    Monitor.prototype.isRunning = function () {
+        return !!this._monitorState.running;
+    };
+    Monitor.prototype.throttle = function (event, handler, wait) {
+        var self = this, _handler = _.wrap(handler, function (fn) {
+            if (self.isRunning()) {
+                fn.apply(this, _.toArray(arguments).slice(1));
+            }
+        });
+        return self.on.call(self, event, _.throttle(_handler, wait || self.config().throttle));
+    };
+    /*
+     * convenience methods
+     */
+    Monitor.prototype._sanitizeNumber = function (n) {
+        if (!_.isNumber(n)) {
+            throw new Error("Number expected");
+        }
+        if (!n || n < 0) {
+            throw new Error("Number must be greater than 0");
+        }
+        // Math.pow(2, 31);
+        if (n >= 2147483648) {
+            throw new Error("Number must be smaller than 2147483648");
+        }
+        return n;
+    };
+    Monitor.prototype.seconds = function (n) {
+        return this._sanitizeNumber(n * 1000);
+    };
+    Monitor.prototype.minutes = function (n) {
+        return this._sanitizeNumber(n * this.seconds(60));
+    };
+    Monitor.prototype.hours = function (n) {
+        return this._sanitizeNumber(n * this.minutes(60));
+    };
+    Monitor.prototype.days = function (n) {
+        return this._sanitizeNumber(n * this.hours(24));
+    };
     return Monitor;
 }(stream.Readable));
-;
 Monitor.prototype.version = '1.0.7';
 // readable stream implementation requirement
 Monitor.prototype._read = function () {
     this._monitorState.streamBuffering = true;
-};
-Monitor.prototype.sendEvent = function (event, obj) {
-    var eventObject = _.extend({ type: event, timestamp: Math.floor(_.now() / 1000) }, obj);
-    // for EventEmitter
-    this.emit(event, eventObject);
-    // for readable Stream
-    if (this.config().stream && this._monitorState.streamBuffering) {
-        var prettyJSON = os.EOL + JSON.stringify(eventObject, null, 2);
-        if (!this.push(prettyJSON)) {
-            this._monitorState.streamBuffering = false;
-        }
-    }
-};
-Monitor.prototype.start = function (options) {
-    var _this = this;
-    if (this._isEnded()) {
-        throw new Error("monitor has been ended by .destroy() method");
-    }
-    this.stop()
-        .config(options);
-    var cycle = function () {
-        var info = {
-            loadavg: os.loadavg(),
-            uptime: os.uptime(),
-            freemem: os.freemem(),
-            totalmem: os.totalmem()
-        }, config = _this.config(), freemem = (config.freemem < 1) ? config.freemem * info.totalmem : config.freemem;
-        if (!config.silent) {
-            _this.sendEvent('monitor', info);
-        }
-        if (info.loadavg[0] > config.critical1) {
-            _this.sendEvent('loadavg1', info);
-        }
-        if (info.loadavg[1] > config.critical5) {
-            _this.sendEvent('loadavg5', info);
-        }
-        if (info.loadavg[2] > config.critical15) {
-            _this.sendEvent('loadavg15', info);
-        }
-        if (info.freemem < freemem) {
-            _this.sendEvent('freemem', info);
-        }
-        if (Number(config.uptime) && info.uptime > Number(config.uptime)) {
-            _this.sendEvent('uptime', info);
-        }
-    };
-    if (this.config().immediate) {
-        process.nextTick(cycle);
-    }
-    this._monitorState.interval = setInterval(cycle, this.config().delay);
-    if (!this.isRunning()) {
-        this._monitorState.running = true;
-        this.sendEvent('start');
-    }
-    return this;
-};
-Monitor.prototype.stop = function () {
-    clearInterval(this._monitorState.interval);
-    if (this.isRunning()) {
-        this._monitorState.running = false;
-        this.sendEvent('stop');
-    }
-    return this;
-};
-Monitor.prototype.reset = function () {
-    this.sendEvent('reset');
-    this[this.isRunning() ? 'start' : 'config'](_.clone(defaults));
-    return this;
-};
-Monitor.prototype.destroy = function (err) {
-    if (!this._isEnded()) {
-        this.sendEvent('destroy');
-        this.stop();
-        if (this instanceof stream.Readable) {
-            this.emit('close');
-            if (_.isFunction(stream.Readable.prototype.destroy)) {
-                stream.Readable.prototype.destroy.apply(this, [err]);
-            }
-            this.push(null);
-        }
-        this._monitorState.ended = true;
-    }
-    return this;
-};
-Monitor.prototype.config = function (options) {
-    if (_.isObject(options)) {
-        _.extend(this._monitorState.config, options);
-        this.sendEvent('config', { options: _.clone(options) });
-    }
-    return this._monitorState.config;
-};
-Monitor.prototype.isRunning = function () {
-    return !!this._monitorState.running;
-};
-Monitor.prototype._isEnded = function () {
-    return !!this._monitorState.ended;
-};
-Monitor.prototype.throttle = function (event, handler, wait) {
-    var self = this, _handler = _.wrap(handler, function (fn) {
-        if (self.isRunning()) {
-            fn.apply(this, _.toArray(arguments).slice(1));
-        }
-    });
-    return self.on.call(self, event, _.throttle(_handler, wait || self.config().throttle));
-};
-/*
- * convenience methods
- */
-Monitor.prototype._sanitizeNumber = function (n) {
-    if (!_.isNumber(n)) {
-        throw new Error("Number expected");
-    }
-    if (!n || n < 0) {
-        throw new Error("Number must be greater than 0");
-    }
-    // Math.pow(2, 31);
-    if (n >= 2147483648) {
-        throw new Error("Number must be smaller than 2147483648");
-    }
-    return n;
-};
-Monitor.prototype.seconds = function (n) {
-    return this._sanitizeNumber(n * 1000);
-};
-Monitor.prototype.minutes = function (n) {
-    return this._sanitizeNumber(n * this.seconds(60));
-};
-Monitor.prototype.hours = function (n) {
-    return this._sanitizeNumber(n * this.minutes(60));
-};
-Monitor.prototype.days = function (n) {
-    return this._sanitizeNumber(n * this.hours(24));
 };
 // expose OS module
 Monitor.prototype.os = os;
--- os-monitor.ts
+++ os-monitor.ts
@@ -22,36 +22,58 @@
 // WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 
-interface InfoObject {
-  loadavg  : number;
-  uptime   : number;
-  freemem  : number;
-  totalmem : number;
+interface ConfigObject {
+  delay?     : number;
+  critical1? : number;
+  critical5? : number;
+  critical15?: number;
+  freemem?   : number;
+  uptime?    : number;
+  silent?    : boolean;
+  stream?    : boolean;
+  immediate? : boolean;
+  throttle?  : number;
 }
 
-let util     = require('util'),
-    os       = require('os'),
-    stream   = require('readable-stream'),
-    _        = require('underscore'),
-    critical = os.cpus().length,
-    defaults = {
-      delay     : 3000,
-      critical1 : critical,
-      critical5 : critical,
-      critical15: critical,
-      freemem   : 0,
-      uptime    : 0,
-      silent    : false,
-      stream    : false,
-      immediate : false
-    };
+interface MonitorState {
+  running: boolean;
+  ended: boolean;
+  streamBuffering: boolean;
+  interval: any;
+  config: ConfigObject;
+}
+
+interface InfoObject {
+  loadavg?  : number;
+  uptime?   : number;
+  freemem?  : number;
+  totalmem? : number;
+  options?  : ConfigObject;
+}
+
+const util     = require('util'),
+      os       = require('os'),
+      stream   = require('readable-stream'),
+      _        = require('underscore'),
+      critical = os.cpus().length,
+      defaults: ConfigObject = {
+        delay     : 3000,
+        critical1 : critical,
+        critical5 : critical,
+        critical15: critical,
+        freemem   : 0,
+        uptime    : 0,
+        silent    : false,
+        stream    : false,
+        immediate : false
+      };
 
 // constructor
 class Monitor extends stream.Readable {
-  private _monitorState: object;
+  private _monitorState: MonitorState;
   public version: string;
   
-  constructor(options?: object) {
+  constructor() {
     super({highWaterMark: 102400});
     this._monitorState = {
       running: false,
@@ -62,9 +84,140 @@ class Monitor extends stream.Readable {
     };
   }
   
+  private sendEvent(event: string, obj?: InfoObject): void {
+
+    let eventObject = _.extend({type: event, timestamp: Math.floor(_.now() / 1000)}, obj);
+    
+    // for EventEmitter
+    this.emit(event, eventObject);
+    // for readable Stream
+    if(this.config().stream && this._monitorState.streamBuffering) {
+      let prettyJSON = os.EOL + JSON.stringify(eventObject, null, 2);
+      if( !this.push(prettyJSON) ) {
+        this._monitorState.streamBuffering = false;
+      }
+    }
+  }
+  
   private _isEnded(): boolean {
     return !!this._monitorState.ended;
   }
+  
+  public start(options?: object) {
+
+      if(this._isEnded()) {
+        throw new Error("monitor has been ended by .destroy() method");
+      }
+
+      this.stop()
+          .config(options);
+
+      let cycle = (): void => {
+        let info: InfoObject = {
+          loadavg  : os.loadavg(),
+          uptime   : os.uptime(),
+          freemem  : os.freemem(),
+          totalmem : os.totalmem()
+        },
+        config = this.config(),
+        freemem  = (config.freemem < 1) ? config.freemem * info.totalmem : config.freemem;
+
+        if(!config.silent) {
+          this.sendEvent('monitor', info);
+        }
+        if(info.loadavg[0] > config.critical1) {
+          this.sendEvent('loadavg1', info);
+        }
+        if(info.loadavg[1] > config.critical5) {
+          this.sendEvent('loadavg5', info);
+        }
+        if(info.loadavg[2] > config.critical15) {
+          this.sendEvent('loadavg15', info);
+        }
+        if(info.freemem < freemem) {
+          this.sendEvent('freemem', info);
+        }
+        if(Number(config.uptime) && info.uptime > Number(config.uptime)) {
+          this.sendEvent('uptime', info);
+        }
+      };
+
+      if(this.config().immediate) {
+        process.nextTick(cycle);
+      }
+      this._monitorState.interval = setInterval(cycle, this.config().delay);
+
+      if(!this.isRunning()) {
+        this._monitorState.running = true;
+        this.sendEvent('start');
+      }
+
+      return this;
+  }
+  
+  public stop() {
+
+    clearInterval(this._monitorState.interval);
+
+    if(this.isRunning()) {
+      this._monitorState.running = false;
+      this.sendEvent('stop');
+    }
+
+    return this;
+  }
+  
+  public reset() {
+    this.sendEvent('reset');
+    this[this.isRunning() ? 'start' : 'config'](_.clone(defaults));
+    return this;
+  }
+  
+  public destroy(err?: any) {
+
+    if(!this._isEnded()) {
+      this.sendEvent('destroy');
+      this.stop();
+      if(this instanceof stream.Readable) {
+        this.emit('close');
+        if(_.isFunction(stream.Readable.prototype.destroy)) {
+          stream.Readable.prototype.destroy.apply(this, [err]);
+        }
+        this.push(null);
+      }
+      this._monitorState.ended = true;
+    }
+
+    return this;
+  }
+  
+  public config(options?: any): ConfigObject {
+
+    if(_.isObject(options)) {
+      _.extend(this._monitorState.config, options);
+      this.sendEvent('config', { options: _.clone(options) });
+    }
+
+    return this._monitorState.config;
+  }
+  
+  public isRunning(): boolean {
+    return !!this._monitorState.running;
+  }
+  
+  public throttle(event: string, handler, wait: number) {
+    let self     = this,
+        _handler = _.wrap(handler, function(fn) {
+                        if(self.isRunning()) {
+                            fn.apply(this, _.toArray(arguments).slice(1));
+                        }
+                    });
+    return self.on.call(self, event, _.throttle(_handler, wait || self.config().throttle));
+  }
+  
+  /*
+   * convenience methods
+   */
   private _sanitizeNumber(n: number): number {
     if(!_.isNumber(n)) {
       throw new Error("Number expected");
@@ -78,7 +231,23 @@ class Monitor extends stream.Readable {
     }
     return n;
   }
-};
+  
+  public seconds(n: number): number {
+    return this._sanitizeNumber(n * 1000);
+  }
+
+  public minutes(n: number): number {
+    return this._sanitizeNumber(n * this.seconds(60));
+  }
+
+  public hours(n: number): number {
+    return this._sanitizeNumber(n * this.minutes(60));
+  }
+
+  public days(n: number): number {
+    return this._sanitizeNumber(n * this.hours(24));
+  }
+}
 
 
 Monitor.prototype.version = '1.0.7';
@@ -88,175 +257,6 @@ Monitor.prototype.version = '1.0.7';
 Monitor.prototype._read = function() {
   this._monitorState.streamBuffering = true;
 };
-
-Monitor.prototype.sendEvent = function(event: string, obj?: InfoObject): void {
-
-  let eventObject = _.extend({type: event, timestamp: Math.floor(_.now() / 1000)}, obj);
-  
-  // for EventEmitter
-  this.emit(event, eventObject);
-  // for readable Stream
-  if(this.config().stream && this._monitorState.streamBuffering) {
-    let prettyJSON = os.EOL + JSON.stringify(eventObject, null, 2);
-    if( !this.push(prettyJSON) ) {
-      this._monitorState.streamBuffering = false;
-    }
-  }
-};
-
-Monitor.prototype.start = function(options: object) {
-
-  if(this._isEnded()) {
-    throw new Error("monitor has been ended by .destroy() method");
-  }
-
-  this.stop()
-      .config(options);
-
-  let cycle = (): void => {
-    let info: InfoObject = {
-      loadavg  : os.loadavg(),
-      uptime   : os.uptime(),
-      freemem  : os.freemem(),
-      totalmem : os.totalmem()
-    },
-    config = this.config(),
-    freemem  = (config.freemem < 1) ? config.freemem * info.totalmem : config.freemem;
-
-    if(!config.silent) {
-      this.sendEvent('monitor', info);
-    }
-    if(info.loadavg[0] > config.critical1) {
-      this.sendEvent('loadavg1', info);
-    }
-    if(info.loadavg[1] > config.critical5) {
-      this.sendEvent('loadavg5', info);
-    }
-    if(info.loadavg[2] > config.critical15) {
-      this.sendEvent('loadavg15', info);
-    }
-    if(info.freemem < freemem) {
-      this.sendEvent('freemem', info);
-    }
-    if(Number(config.uptime) && info.uptime > Number(config.uptime)) {
-      this.sendEvent('uptime', info);
-    }
-  };
-  
-  if(this.config().immediate) {
-    process.nextTick(cycle);
-  }
-  this._monitorState.interval = setInterval(cycle, this.config().delay);
-
-  if(!this.isRunning()) {
-    this._monitorState.running = true;
-    this.sendEvent('start');
-  }
-
-  return this;
-};
-
-Monitor.prototype.stop = function() {
-
-  clearInterval(this._monitorState.interval);
-
-  if(this.isRunning()) {
-    this._monitorState.running = false;
-    this.sendEvent('stop');
-  }
-
-  return this;
-};
-
-Monitor.prototype.reset = function() {
-  this.sendEvent('reset');
-  this[this.isRunning() ? 'start' : 'config'](_.clone(defaults));
-  return this;
-};
-
-Monitor.prototype.destroy = function(err?: any) {
-
-  if(!this._isEnded()) {
-    this.sendEvent('destroy');
-    this.stop();
-    if(this instanceof stream.Readable) {
-      this.emit('close');
-      if(_.isFunction(stream.Readable.prototype.destroy)) {
-        stream.Readable.prototype.destroy.apply(this, [err]);
-      }
-      this.push(null);
-    }
-    this._monitorState.ended = true;
-  }
-
-  return this;
-};
-
-Monitor.prototype.config = function(options?: object): object {
-
-  if(_.isObject(options)) {
-    _.extend(this._monitorState.config, options);
-    this.sendEvent('config', { options: _.clone(options) });
-  }
-
-  return this._monitorState.config;
-};
-
-Monitor.prototype.isRunning = function(): boolean {
-  return !!this._monitorState.running;
-};
-
-//Monitor.prototype._isEnded = function(): boolean {
-//  return !!this._monitorState.ended;
-//};
-
-Monitor.prototype.throttle = function(event: string, handler, wait: number) {
-  let self     = this,
-      _handler = _.wrap(handler, function(fn) {
-                   if(self.isRunning()) {
-                     fn.apply(this, _.toArray(arguments).slice(1));
-                   }
-                 });
-  return self.on.call(self, event, _.throttle(_handler, wait || self.config().throttle));
-};
-
-
-/*
- * convenience methods
- */
-
-/*
-Monitor.prototype._sanitizeNumber = function(n: number): number {
-  if(!_.isNumber(n)) {
-    throw new Error("Number expected");
-  }
-  if(!n || n < 0) {
-    throw new Error("Number must be greater than 0");
-  }
-  // Math.pow(2, 31);
-  if(n >= 2147483648) {
-    throw new Error("Number must be smaller than 2147483648");
-  }
-  return n;
-};
-*/
-
-Monitor.prototype.seconds = function(n: number): number {
-  return this._sanitizeNumber(n * 1000);
-};
-
-Monitor.prototype.minutes = function(n: number): number {
-  return this._sanitizeNumber(n * this.seconds(60));
-};
-
-Monitor.prototype.hours = function(n: number): number {
-  return this._sanitizeNumber(n * this.minutes(60));
-};
-
-Monitor.prototype.days = function(n: number): number {
-  return this._sanitizeNumber(n * this.hours(24));
-};
-
 
 // expose OS module
 Monitor.prototype.os = os;
